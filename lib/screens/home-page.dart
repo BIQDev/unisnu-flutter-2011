056383ee3877c11c61495b9b00060e49c4bbe624
Bayu Candra <bayucandra@gmail.com>
2020-12-20 20:08:39 +0700

    (C)reate then (R)ead

--- lib/screens/home-page.dart
+++ lib/screens/home-page.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import 'package:perpus/screens/book-input.dart';
 import 'package:perpus/widgets/home/book-list.dart';
 import 'package:perpus/widgets/home/header.dart';
+import 'package:perpus/providers/booklist_provider.dart';
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key key}) : super(key: key);
 
   _newBook(BuildContext addButtonContext) async {
-    Navigator.of(addButtonContext).pushNamed(
+    final result = await Navigator.of(addButtonContext).pushNamed(
       BookInputScreen.routeName,
     );
+
+    if (result == 200) {
+      Scaffold.of(addButtonContext)
+        ..removeCurrentSnackBar()
+        ..showSnackBar(
+          SnackBar(
+            content: Text("Berhasil menambah buku"),
+            backgroundColor: Colors.greenAccent.shade700,
+          ),
+        );
+      addButtonContext.read<BookListProvider>().read(addButtonContext);
+    }
   }
 
   @override
